0d8ce5ff90a9139acde2c669a34b5493be06aaec
adhityasan <adhityad3v@gmail.com>
2019-04-07 05:13:41 +0700

    init commit

---
+++ controller/controller.go
@@ -0,0 +1,192 @@
+package controller
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/adhityasan/ekyc-api/imagehandler"
+	"github.com/adhityasan/ekyc-api/pii"
+	"github.com/adhityasan/ekyc-api/pii/assigner"
+	"github.com/disintegration/gift"
+)
+
+type controllerResp struct {
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func writeRespByte(msg string, data interface{}) []byte {
+	var resp controllerResp
+	if len(msg) == 0 {
+		msg = "Success"
+	}
+	resp.Message = msg
+	resp.Data = data
+	res, _ := json.Marshal(resp)
+
+	return res
+}
+
+// var cuttariImg = `{"url":"http://cdn2.tstatic.net/batam/foto/bank/images/cut-tari-artis-dan-pembawa-acara-televisi.jpg"}`
+// var ersamayoriImg = `{"url":"https://cdns.klimg.com/kapanlagi.com/selebriti/Ersa_Mayori/p/ersa-mayori-025.jpg"}`
+
+// IdentifyByAzure identify endpoint using azure face comparation
+// also `pii.Save()` for saving data to database
+func IdentifyByAzure(w http.ResponseWriter, r *http.Request) {
+
+	decodedPii, errDecode := pii.DecodeFormPost(r)
+
+	if errDecode != nil {
+		log.Println(errDecode)
+	}
+
+	piiExist, errExist := decodedPii.Exist()
+
+	if errExist != nil {
+		log.Println(errExist)
+	}
+
+	asyncProc := make(chan interface{}, 1)
+
+	if piiExist == false {
+		go func() {
+			id, errSave := decodedPii.Save()
+			if errSave != nil {
+				log.Println(errSave)
+			}
+			asyncProc <- id
+		}()
+	}
+
+	adapter := imagehandler.AzureAdapter{}
+	go adapter.Compare(decodedPii.PasfotoKTP.Data, decodedPii.FotoSelfie.Data, asyncProc)
+	confidence := <-asyncProc
+
+	w.Write([]byte(fmt.Sprintf("%v", confidence)))
+}
+
+// Identify as endpoint
+func Identify(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(fmt.Sprintf("Your submition is completed. Thank you~ ulalaa~")))
+	log.Println("Processing Data")
+	piiChan := make(chan interface{}, 1)
+	compImgChan := make(chan interface{}, 1)
+
+	if decodedPii, errDecode := pii.DecodeFormPost(r); errDecode == nil {
+		log.Println("Processing Images")
+		adapter := &imagehandler.AwsAdapter{}
+		go adapter.Compare(decodedPii.PasfotoKTP.Data, decodedPii.FotoSelfie.Data, compImgChan)
+		confidence := <-compImgChan
+
+		log.Println("Check if NIK " + decodedPii.Nik + " exist")
+		_, errExist := decodedPii.Exist()
+		if errExist != nil {
+			log.Println("Get data from dukcapil")
+			formatted := new(pii.Pii)
+			data := bytes.NewReader(HitDukcapil(decodedPii.Nik))
+			json.NewDecoder(data).Decode(&formatted)
+			formatted.FotoKTP = decodedPii.FotoKTP
+			formatted.FotoSelfie = decodedPii.FotoSelfie
+			formatted.PasfotoKTP = decodedPii.PasfotoKTP
+			go func() {
+				id, errSave := formatted.Save()
+				if errSave != nil {
+					fmt.Println(errSave)
+				}
+				log.Println("Data saved")
+				piiChan <- id
+			}()
+			decodedPii = formatted
+		}
+		savedID := <-piiChan
+		log.Println("Processing data finished.\nResult :" + fmt.Sprint(confidence) + "\n" + fmt.Sprint(savedID))
+	} else {
+		fmt.Println(errDecode)
+	}
+}
+
+// HitDukcapil function hit Dukcapil simulator API from Docker IP
+func HitDukcapil(nik string) (data []byte) {
+	dukcapilURI := "http://192.168.99.100:5000"
+	param := `{"NIK": "` + nik + `"}`
+	var decoded struct {
+		Content []map[string]interface{} `json:"content"`
+	}
+	req, _ := http.NewRequest("POST", dukcapilURI, strings.NewReader(param))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	if resp, err := client.Do(req); err == nil {
+		json.NewDecoder(resp.Body).Decode(&decoded)
+		if data, err = json.Marshal(decoded.Content[0]); err == nil {
+			return data
+		}
+		return []byte(fmt.Sprintln(err))
+	}
+	return nil
+}
+
+// Aisatsu sample get request for testing purpose
+func Aisatsu(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Guest"
+	}
+	log.Printf("こんにちは %s'san\n", name)
+	w.Write([]byte(fmt.Sprintf("Hello, %s'san\n", name)))
+}
+
+// DoOCR for KTP Image as first endpoint
+func DoOCR(w http.ResponseWriter, r *http.Request) {
+	// Sementara pakai buffer, next pakai Pii untuk return objectID
+	bufKTP := bytes.NewBuffer(nil)
+	imgChan := make(chan interface{})
+	if imgKTP, headerKTP, err := r.FormFile("foto_ktp"); err == nil {
+		fmt.Println("Reading Image " + headerKTP.Filename)
+		defer imgKTP.Close()
+		img, _ := jpeg.Decode(imgKTP)
+		g := gift.New(
+			gift.Contrast(20),
+			gift.Grayscale(),
+		)
+		imgEnhance := image.NewRGBA(g.Bounds(img.Bounds()))
+		g.Draw(imgEnhance, img)
+		err = jpeg.Encode(bufKTP, imgEnhance, nil)
+		adapter := &imagehandler.AwsAdapter{}
+		go adapter.Read(bufKTP.Bytes(), imgChan)
+		ocrRes := <-imgChan
+		w.Write([]byte(fmt.Sprintf("%v", ocrRes)))
+	} else {
+		w.Write([]byte(fmt.Sprintln(err)))
+	}
+}
+
+// AssignFakePii to assign new fake data to Pii collection
+func AssignFakePii(response http.ResponseWriter, request *http.Request) {
+
+	var person pii.Pii
+
+	json.NewDecoder(request.Body).Decode(&person)
+	err := assigner.Assigner(person.Nik, &person)
+	if err != nil {
+		log.Println(err)
+		response.Write(writeRespByte(err.Error(), person))
+		return
+	}
+
+	_, errSave := person.Save()
+	if errSave != nil {
+		log.Println(errSave)
+		response.Write(writeRespByte(errSave.Error(), person))
+		return
+	}
+
+	response.Write(writeRespByte("", person))
+}
---
+++ pii/pii.go
@@ -0,0 +1,160 @@
+package pii
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/adhityasan/ekyc-api/config"
+
+	"github.com/adhityasan/ekyc-api/pii/piimage"
+
+	"github.com/gorilla/schema"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var dbhost = config.Of.Mongo.Host
+var dbport = config.Of.Mongo.Port
+var dburl = config.Of.Mongo.URL
+var dbname = config.Of.DBModules["pii"].Db
+var dbcoll = config.Of.DBModules["pii"].Coll
+
+// Pii stands for Personal Identifying Information
+type Pii struct {
+	ID                primitive.ObjectID   `schema:"_id" bson:"_id,omitempty" json:"_id,omitempty"`
+	Nik               string               `schema:"NIK,omitempty" bson:"nik,omitempty" json:"NIK,omitempty"`
+	EktpStatus        bool                 `schema:"EKTP_STATUS,omitempty" bson:"ektp_status,omitempty" json:"EKTP_STATUS,omitempty"`
+	NamaLengkap       string               `schema:"NAMA_LENGKAP,omitempty" bson:"nama_lengkap,omitempty" json:"NAMA_LENGKAP,omitempty"`
+	NamaLengkapIbu    string               `schema:"NAMA_LENGKAP_IBU,omitempty" bson:"nama_lengkap_ibu,omitempty" json:"NAMA_LENGKAP_IBU,omitempty"`
+	NoHp              string               `schema:"NOMOR_HANDPHONE,omitempty" bson:"nomor_handphone,omitempty" json:"NOMOR_HANDPHONE,omitempty"`
+	TanggalLahir      string               `schema:"TANGGAL_LAHIR,omitempty" bson:"tanggal_lahir,omitempty" json:"TANGGAL_LAHIR,omitempty"`
+	TempatLahir       string               `schema:"TEMPAT_LAHIR,omitempty" bson:"tempat_lahir,omitempty" json:"TEMPAT_LAHIR,omitempty"`
+	PendidikanAkhir   string               `schema:"PENDIDIKAN_AKHIR,omitempty" bson:"pendidikan_akhir,omitempty" json:"PENDIDIKAN_AKHIR,omitempty"`
+	NoKK              string               `schema:"NOMOR_KARTU_KELUARGA,omitempty" bson:"nomor_kartu_keluarga,omitempty" json:"NOMOR_KARTU_KELUARGA,omitempty"`
+	Alamat            string               `schema:"ALAMAT,omitempty" bson:"alamat,omitempty" json:"ALAMAT,omitempty"`
+	Rt                string               `schema:"RT,omitempty" bson:"rt,omitempty" json:"RT,omitempty"`
+	Rw                string               `schema:"RW,omitempty" bson:"rw,omitempty" json:"RW,omitempty"`
+	NomorKelurahan    string               `schema:"NOMOR_KELURAHAN,omitempty" bson:"nomor_kelurahan,omitempty" json:"NOMOR_KELURAHAN,omitempty"`
+	Kelurahan         string               `schema:"KELURAHAN,omitempty" bson:"kelurahan,omitempty" json:"KELURAHAN,omitempty"`
+	NomorKecamatan    string               `schema:"NOMOR_KECAMATAN,omitempty" bson:"nomor_kecamatan,omitempty" json:"NOMOR_KECAMATAN,omitempty"`
+	Kecamatan         string               `schema:"KECAMATAN,omitempty" bson:"kecamatan,omitempty" json:"KECAMATAN,omitempty"`
+	NomorKabupaten    string               `schema:"NOMOR_KABUPATEN,omitempty" bson:"nomor_kabupaten,omitempty" json:"NOMOR_KABUPATEN,omitempty"`
+	Kabupaten         string               `schema:"KABUPATEN,omitempty" bson:"kabupaten,omitempty" json:"KABUPATEN,omitempty"`
+	NomorProvinsi     string               `schema:"NOMOR_PROVINSI,omitempty" bson:"nomor_provinsi,omitempty" json:"NOMOR_PROVINSI,omitempty"`
+	Provinsi          string               `schema:"PROVINSI,omitempty" bson:"provinsi,omitempty" json:"PROVINSI,omitempty"`
+	Agama             string               `schema:"AGAMA,omitempty" bson:"agama,omitempty" json:"AGAMA,omitempty"`
+	Pekerjaan         string               `schema:"PEKERJAAN,omitempty" bson:"pekerjaan,omitempty" json:"PEKERJAAN,omitempty"`
+	JenisKelamin      string               `schema:"JENIS_KELAMIN,omitempty" bson:"jenis_kelamin,omitempty" json:"JENIS_KELAMIN,omitempty"`
+	StatusPerkawinan  string               `schema:"STATUS_PERKAWINAN,omitempty" bson:"status_perkawinan,omitempty" json:"STATUS_PERKAWINAN,omitempty"`
+	FotoKTP           *piimage.ImageStruct `schema:"FOTO_KTP,omitempty" bson:"foto_ktp,omitempty" json:"FOTO_KTP,omitempty"`
+	FotoSelfie        *piimage.ImageStruct `schema:"FOTO_SELFIE,omitempty" bson:"foto_selfie,omitempty" json:"FOTO_SELFIE,omitempty"`
+	FotoSelfieWithKTP *piimage.ImageStruct `schema:"FOTO_SELFIE_KTP" bson:"foto_selfie_ktp,omitempty" json:"FOTO_SELFIE_KTP,omitempty"`
+	PasfotoKTP        *piimage.ImageStruct `schema:"PASFOTO_KTP,omitempty" bson:"pasfoto_ktp,omitempty" json:"PASFOTO_KTP,omitempty"`
+}
+
+func openPiiCollection() (context.Context, context.CancelFunc, *mongo.Client, *mongo.Collection, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(dburl))
+	collection := client.Database(dbname).Collection(dbcoll)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	return ctx, cancel, client, collection, err
+}
+
+// DecodeFormPost decode the formPost data in requests form-data and assign it to Pii Struct
+func DecodeFormPost(r *http.Request) (*Pii, error) {
+
+	r.ParseMultipartForm(10 << 20)
+
+	fd := r.PostForm
+	newPii := new(Pii)
+	decoder := schema.NewDecoder()
+	decoder.IgnoreUnknownKeys(true)
+
+	if fd.Get("TANGGAL_LAHIR") != "" {
+		parsedtgllahir, errParse := time.Parse("2006-01-02", fd.Get("TANGGAL_LAHIR"))
+		if errParse != nil {
+			return nil, errors.New("Cannot parse tanggallahir decode")
+		}
+		tgllahirString := parsedtgllahir.String()
+		fd.Set("TANGGAL_LAHIR", tgllahirString)
+	}
+
+	err := decoder.Decode(newPii, fd)
+
+	if err != nil {
+		errdetail := fmt.Sprintf("Fail to decode request form-data to new Pii data Struct : %s\n", err)
+		return nil, errors.New(errdetail)
+	}
+
+	newPii.FotoKTP, err = piimage.ImageStructHandler("FOTO_KTP", r)
+	newPii.FotoSelfie, err = piimage.ImageStructHandler("FOTO_SELFIE", r)
+	newPii.FotoSelfieWithKTP, err = piimage.ImageStructHandler("FOTO_SELFIE_KTP", r)
+	newPii.PasfotoKTP, err = piimage.ImageStructHandler("PASFOTO_KTP", r)
+
+	return newPii, nil
+}
+
+// Save current Personal Identifying Information
+func (p *Pii) Save() (interface{}, error) {
+
+	exist, _ := p.Exist()
+	if exist {
+		return nil, errors.New("Pii data exist, Pii.ID has been set")
+	}
+
+	ctx, cancel, _, collection, _ := openPiiCollection()
+	res, err := collection.InsertOne(ctx, p)
+	defer cancel()
+
+	if err != nil {
+		return nil, err
+	}
+
+	newid := &p.ID
+	*newid = res.InsertedID.(primitive.ObjectID)
+
+	return p.ID, nil
+}
+
+// Exist Check Pii data existance in local database
+func (p *Pii) Exist() (bool, error) {
+	_, cancel, _, collection, _ := openPiiCollection()
+	decodepoint := new(Pii)
+	err := collection.FindOne(context.TODO(), bson.M{"nik": p.Nik}).Decode(decodepoint)
+	defer cancel()
+
+	if err != nil {
+		log.Println(err)
+		return false, err
+	}
+
+	pointerID := &p.ID
+	*pointerID = decodepoint.ID
+
+	return true, nil
+}
+
+// GrepData gerp all pii data by it current _id or nik
+func (p *Pii) GrepData() error {
+	_, cancel, _, collection, _ := openPiiCollection()
+	decodepoint := new(Pii)
+	err := collection.FindOne(context.TODO(), bson.M{"nik": p.Nik}).Decode(decodepoint)
+	defer cancel()
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%v\n", decodepoint)
+	return nil
+}
