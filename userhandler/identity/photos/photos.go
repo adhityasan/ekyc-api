2454d5bf848e4175173e9b5d3915447fd1d4dac2
adhityasan <adhityad3v@gmail.com>
2019-04-08 03:55:57 +0700

    PhotoStruct remove Header attr

--- userhandler/identity/photos/photos.go
+++ userhandler/identity/photos/photos.go
@@ -4,15 +4,13 @@ import (
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
-	"net/textproto"
 )
 
 // PhotoStruct standart Struct for image
 type PhotoStruct struct {
-	Data   []byte               `schema:"data,omitempty" bson:"data,omitempty"`
-	Name   string               `schema:"name,omitempty" bson:"name,omitempty"`
-	Size   int64                `schema:"size,omitempty" bson:"size,omitempty"`
-	Header textproto.MIMEHeader `schema:"header,omitempty" bson:"header,omitempty"`
+	Data []byte `schema:"data,omitempty" bson:"data,omitempty"`
+	Name string `schema:"name,omitempty" bson:"name,omitempty"`
+	Size int64  `schema:"size,omitempty" bson:"size,omitempty"`
 }
 
 // PhotoStructHandler create image Struct with Data , Name, Size, Header from a request
@@ -30,7 +28,6 @@ func PhotoStructHandler(fieldname string, r *http.Request) (*PhotoStruct, multip
 	newPhotoStruct.Data = fileBytes
 	newPhotoStruct.Name = handler.Filename
 	newPhotoStruct.Size = handler.Size
-	newPhotoStruct.Header = handler.Header
 
 	return newPhotoStruct, file, nil
 }
