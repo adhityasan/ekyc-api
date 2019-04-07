ab1afb7bb5eabb4e3e1940ad0ff207dd1f5d961f
adhityasan <adhityad3v@gmail.com>
2019-04-07 08:13:22 +0700

    PhotoStruct

---
+++ userhandler/identity/photos/photos.go
@@ -0,0 +1,35 @@
+package photos
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/textproto"
+)
+
+// PhotoStruct standart Struct for image
+type PhotoStruct struct {
+	Data   []byte               `schema:"data,omitempty" bson:"data,omitempty"`
+	Name   string               `schema:"name,omitempty" bson:"name,omitempty"`
+	Size   int64                `schema:"size,omitempty" bson:"size,omitempty"`
+	Header textproto.MIMEHeader `schema:"header,omitempty" bson:"header,omitempty"`
+}
+
+// PhotoStructHandler create image Struct with Data , Name, Size, Header from a request
+func PhotoStructHandler(fieldname string, r *http.Request) (*PhotoStruct, error) {
+	file, handler, err := r.FormFile(fieldname)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	newPhotoStruct := new(PhotoStruct)
+	newPhotoStruct.Data = fileBytes
+	newPhotoStruct.Name = handler.Filename
+	newPhotoStruct.Size = handler.Size
+	newPhotoStruct.Header = handler.Header
+
+	return newPhotoStruct, nil
+}
